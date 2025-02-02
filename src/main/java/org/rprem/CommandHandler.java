9e1b8f50736460c1c381afcbf270075734239068
TheRayZV <112191289+TheRayZV@users.noreply.github.com>
2025-02-02 23:07:26 +0800

    CommandHandler.java

--- src/main/java/org/rprem/CommandHandler.java
+++ src/main/java/org/rprem/CommandHandler.java
@@ -28,40 +28,46 @@ public class CommandHandler implements CommandExecutor {
 
         String action = args[0];
         String playerName = args[1];
-        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerName);
 
-        if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) {
-            sender.sendMessage("Игрок " + playerName + " не найден в базе данных.");
-            return true;
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
 
-        UUID playerUUID = offlinePlayer.getUniqueId();
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(playerName);
 
-        if (action.equalsIgnoreCase("add")) {
-            if (args.length < 3) {
-                sender.sendMessage(plugin.getConfig().getString("messages.invalid-command"));
-                return true;
+            if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) {
+                sender.sendMessage("Игрок " + playerName + " не найден в базе данных.");
+                return;
             }
 
-            String time = args[2];
-            long duration = TimeUtils.parseTime(time);
+            UUID playerUUID = offlinePlayer.getUniqueId();
 
-            if (duration == -1) {
-                sender.sendMessage(plugin.getConfig().getString("messages.invalid-time-format"));
-                return true;
+            switch (action.toLowerCase()) {
+                case "add" -> {
+                    if (args.length < 3) {
+                        sender.sendMessage(plugin.getConfig().getString("messages.invalid-command"));
+                        return;
+                    }
+
+                    String time = args[2];
+                    long duration = TimeUtils.parseTime(time);
+
+                    if (duration == -1) {
+                        sender.sendMessage(plugin.getConfig().getString("messages.invalid-time-format"));
+                        return;
+                    }
+
+                    subscriptionManager.addSubscription(playerUUID, duration);
+                    sender.sendMessage(plugin.getConfig().getString("messages.subscription-added")
+                            .replace("%player%", playerName)
+                            .replace("%time%", time));
+                }
+                case "rem" -> {
+                    subscriptionManager.removeSubscription(playerUUID);
+                    sender.sendMessage(plugin.getConfig().getString("messages.subscription-removed")
+                            .replace("%player%", playerName));
+                }
+                default -> sender.sendMessage(plugin.getConfig().getString("messages.invalid-command"));
             }
-
-            subscriptionManager.addSubscription(playerUUID, duration);
-            sender.sendMessage(plugin.getConfig().getString("messages.subscription-added")
-                    .replace("%player%", playerName)
-                    .replace("%time%", time));
-        } else if (action.equalsIgnoreCase("rem")) {
-            subscriptionManager.removeSubscription(playerUUID);
-            sender.sendMessage(plugin.getConfig().getString("messages.subscription-removed")
-                    .replace("%player%", playerName));
-        } else {
-            sender.sendMessage(plugin.getConfig().getString("messages.invalid-command"));
-        }
+        });
 
         return true;
     }
